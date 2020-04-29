68d14d4d6bc1f4e8a9ed52ca9b1c7257c8ea83d7
semenInRussia <semenprofis999@gmail.com>
2020-04-29 17:56:27 +0300

    добавлен коммент в settings.py, к доступу в базу данных

--- DistanceLerning/DistanceLerning/settings.py
+++ DistanceLerning/DistanceLerning/settings.py
@@ -69,6 +69,17 @@ WSGI_APPLICATION = 'DistanceLerning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'distance_lerning',
+#         'USER': 'semen',
+#         'PASSWORD': 'feijrghrugtrpehrfeihrfgipeai01',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
